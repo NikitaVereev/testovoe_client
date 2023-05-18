c9dd592ed590d9a042112416a67a05e0e518f6e0
Nikita <nikitkavershinin2019@gmail.com>
2023-05-18 21:35:29 +0300

    v

--- src/components/screens/create-post/CreatePost.tsx
+++ src/components/screens/create-post/CreatePost.tsx
@@ -66,7 +66,7 @@ const CreatePost: FC = () => {
 		createPost({
 			message: isMessage,
 
-			media: `https://testovoeserver-production.up.railway.app/api/uploads/${media}`,
+			media: `https://testovoeserver-production.up.railway.app/api${media}`,
 		})
 		console.log(media, 'dfdf')
 	}
